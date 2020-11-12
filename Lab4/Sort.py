641730444bf2f41f43bab77882f58547166d301b
Zbigniew "Radagast" Kaleta <zkaleta@agh.edu.pl>
2020-11-12 20:14:33 +0100

    Komentarz do lab 3

--- Lab4/Sort.py
+++ Lab4/Sort.py
@@ -4,20 +4,20 @@ def insertion_sort(array):
         currentPosition = i
         while currentPosition > 0 and array[currentPosition - 1] > currentValue:
             array[currentPosition] = array[currentPosition - 1]
-            currentPosition = currentPosition - 1
+            currentPosition = currentPosition - 1   # -= 1
         array[currentPosition] = currentValue
 
 
 def selection_sort(array):
     n = len(array)
     for i in range(n):
-        minimum = i
+        minimum = i # sugeruję min_index, albo argmin, dla odróżnienia, że to nie jest najmniejsza wartość, tylko indeks pod którym się znajduje
         for j in range(i + 1, n):
             if (array[j] < array[minimum]):
                 minimum = j
         temp = array[i]
         array[i] = array[minimum]
-        array[minimum] = temp
+        array[minimum] = temp   # a, b = b, a
     return array
 
 
--- Lab4/Text.py
+++ Lab4/Text.py
@@ -1,6 +1,6 @@
-from collections import *
+from collections import *   # nie zaleca się stosowania tej formy importu
 
-PUNCTATIONS = ".,?!-:;()\""
+PUNCTATIONS = ".,?!-:;()\"" # literówka
 forms_in_text = {}
 digrams_in_text = {}
 trigrams_in_text = {}
@@ -23,7 +23,7 @@ def get_forms_from_text(filepath):
 
 def get_digrams_from_text(filepath):
     new_digram = []
-    with open(filepath, "r", encoding="utf-8") as infile:
+    with open(filepath, "r", encoding="utf-8") as infile:   # DRY - patrz funkcja wyżej
         for line in infile:
             line = line.strip()
             for punct in PUNCTATIONS:
@@ -40,11 +40,11 @@ def get_digrams_from_text(filepath):
                             new_digram.pop(1)
                         else:
                             digrams_in_text[str(new_digram)] = 1
-                            new_digram[0] = new_digram[1]
+                            new_digram[0] = new_digram[1]   # DRY - wyciąganie wspólnego czynnika za nawias
                             new_digram.pop(1)
 
 
-def get_trigrams_from_text(filepath):
+def get_trigrams_from_text(filepath):   # czy dałoby się tę funkcję uogólnić na n-gramy?
     new_trigram = []
     with open(filepath, "r", encoding="utf-8") as infile:
         for line in infile:
@@ -60,11 +60,12 @@ def get_trigrams_from_text(filepath):
                         if str(new_trigram) in trigrams_in_text.keys():
                             trigrams_in_text[str(new_trigram)] += 1
                             new_trigram[0] = new_trigram[1]
-                            new_trigram.pop(1)
+                            new_trigram.pop(1)  # czemu pop(1)?
                         else:
                             trigrams_in_text[str(new_trigram)] = 1
                             new_trigram[0] = new_trigram[1]
                             new_trigram.pop(1)
+
 
 
 def sort_elements_in_dictionary(dictionary):
@@ -77,7 +78,7 @@ print(sort_elements_in_dictionary(forms_in_text))
 
 
 get_digrams_from_text("potop.txt")
-print(Counter(digrams_in_text).most_common(20))
+print(Counter(digrams_in_text).most_common(20)) # to nie gwarantuje wypisania remisów
 
 
 
