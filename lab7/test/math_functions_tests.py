e0b1de2aa0566099d21628c370e07c2a07898c6d
bklimza <72684436+bklimza@users.noreply.github.com>
2020-11-28 20:36:59 +0100

    Add files via upload

---
+++ lab7/test/math_functions_tests.py
@@ -0,0 +1,33 @@
+import unittest
+
+from main.math_functions import *
+
+
+class FindStraightLineEquationTest(unittest.TestCase):
+    def test_incorrect_input(self):
+        self.assertRaises(ValueError, find_straight_line_equation, (0, 0), (0, 0))
+
+    def test_correct_input(self):
+        self.assertCountEqual([1, 0], list(find_straight_line_equation((1, 1), (2, 2))))
+        self.assertCountEqual([2.5, 5], list(find_straight_line_equation((0, 5), (5, 17.5))))
+
+
+class SolveQuadraticEquationTest(unittest.TestCase):
+    def test_incorrect_input(self):
+        self.assertRaises(ValueError, solve_quadratic_equation, 0, 0, 0)
+
+    def test_delta_negative(self):
+        self.assertCountEqual({}, solve_quadratic_equation(2, 0, 3))
+        self.assertCountEqual({}, solve_quadratic_equation(3, 0, 5))
+
+    def test_delta_zero(self):
+        self.assertCountEqual({-1}, solve_quadratic_equation(2, 4, 2))
+        self.assertCountEqual({1}, solve_quadratic_equation(-3, 6, -3))
+
+    def test_delta_positive(self):
+        self.assertCountEqual({1, -1}, solve_quadratic_equation(1, 0, -1))
+        self.assertCountEqual({2, -3}, solve_quadratic_equation(3, 3, -18))
+
+
+if __name__ == "__main__":
+    unittest.main()
