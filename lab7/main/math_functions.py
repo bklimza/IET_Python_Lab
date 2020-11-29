c4e74d716888c1f3908fb067c037c0a10dd65b66
bklimza <72684436+bklimza@users.noreply.github.com>
2020-11-29 20:05:33 +0100

    Update math_functions.py

--- lab7/main/math_functions.py
+++ lab7/main/math_functions.py
@@ -1,10 +1,18 @@
+def check_if_number(number):
+    if not isinstance(number, (int, float)):
+        raise TypeError("Please check numbers")
+
+
 def solve_quadratic_equation(a, b, c):
+    for number in (a, b, c):
+        check_if_number(number)
+
     if a == b == c == 0:
         raise ValueError("Every coefficient equals zero")
     if a == 0:
         return {-c / b}  # linear equation
     if a == b == 0:
-        raise ZeroDivisionError
+        raise ZeroDivisionError("Contradictory equation")
     delta = b ** 2 - 4 * a * c
     if delta == 0:
         return {-b / (2 * a)}
@@ -15,6 +23,12 @@ def solve_quadratic_equation(a, b, c):
 
 
 def find_straight_line_equation(point1, point2):
+    for point in (point1, point2):
+        if len(point) == 2:
+            for coordinate in point:
+                check_if_number(coordinate)
+        else:
+            raise TypeError("Please check numbers")
     if point1[0] == point2[0] and point1[1] == point2[1]:
         raise ValueError("Given points are equal")
     return (point2[1] - point1[1]) / (point2[0] - point1[0]),\
