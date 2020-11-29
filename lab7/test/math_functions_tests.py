20b908abf8c4a45ddfbddade52a0eac9c669e5d2
bklimza <72684436+bklimza@users.noreply.github.com>
2020-11-29 12:11:54 +0100

    Update math_functions_tests.py

--- lab7/test/math_functions_tests.py
+++ lab7/test/math_functions_tests.py
@@ -28,6 +28,13 @@ class SolveQuadraticEquationTest(unittest.TestCase):
         self.assertCountEqual({1, -1}, solve_quadratic_equation(1, 0, -1))
         self.assertCountEqual({2, -3}, solve_quadratic_equation(3, 3, -18))
 
+    def test_a_coefficient_zero(self):
+        self.assertCountEqual({1}, solve_quadratic_equation(0, 2, -2))
+        self.assertCountEqual({-2}, solve_quadratic_equation(0, 3, 6))
+
+    def test_zero_division(self):
+        self.assertRaises(ZeroDivisionError, solve_quadratic_equation, 0, 0, 4)
+
 
 if __name__ == "__main__":
     unittest.main()
