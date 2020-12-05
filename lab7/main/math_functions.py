954378d377383f823d18e855e0b0b32a7a9d6519
Zbigniew "Radagast" Kaleta <zkaleta@agh.edu.pl>
2020-12-05 19:30:18 +0100

    Komentarz do lab 6

--- lab7/main/math_functions.py
+++ lab7/main/math_functions.py
@@ -8,11 +8,11 @@ def solve_quadratic_equation(a, b, c):
         check_if_number(number)
 
     if a == b == c == 0:
-        raise ValueError("Every coefficient equals zero")
+        raise ValueError("Every coefficient equals zero")   # wniosek?
     if a == 0:
         return {-c / b}  # linear equation
     if a == b == 0:
-        raise ZeroDivisionError("Contradictory equation")
+        raise ZeroDivisionError("Contradictory equation")   # czym się różni równanie liniowe bez rozwiązań od kwadratowego bez rozwiązań?
     delta = b ** 2 - 4 * a * c
     if delta == 0:
         return {-b / (2 * a)}
@@ -28,8 +28,8 @@ def find_straight_line_equation(point1, point2):
             for coordinate in point:
                 check_if_number(coordinate)
         else:
-            raise TypeError("Please check numbers")
+            raise TypeError("Please check numbers") # mylący komunikat
     if point1[0] == point2[0] and point1[1] == point2[1]:
-        raise ValueError("Given points are equal")
-    return (point2[1] - point1[1]) / (point2[0] - point1[0]),\
+        raise ValueError("Given points are equal")  # wniosek?
+    return (point2[1] - point1[1]) / (point2[0] - point1[0]),\  # ZeroDivisionError
         point1[1] - point1[0] * (point2[1] - point1[1]) / (point2[0] - point1[0])
