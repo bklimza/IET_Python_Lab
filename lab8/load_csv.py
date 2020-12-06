0a64e055e1e658025ddea860c63caee4b4f60c5e
bklimza <72684436+bklimza@users.noreply.github.com>
2020-12-06 17:54:52 +0100

    Update load_csv.py

--- lab8/load_csv.py
+++ lab8/load_csv.py
@@ -30,7 +30,7 @@ def normalize_column(number, data_matrix):
             i += 1
 
 
-def normalize_rows(data_matrix): 
+def normalize_all_rows(data_matrix):
     for i in range(number_of_columns(data_matrix)):
         normalize_column(i, data_matrix)
 
@@ -42,6 +42,11 @@ def center_column(number, data_matrix):
     for _ in data_matrix[:, number]:
         data_matrix[:, number][i] -= col_mean
         i += 1
+
+
+def center_all_rows(data_matrix):
+    for i in range(number_of_columns(data_matrix)):
+        center_column(i, data_matrix)
 
 
 def divide_data_to_train_and_test(data_matrix, labels,  how_many_rows_to_train):
