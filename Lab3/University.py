3325cf94e0191ca2a0a99e3d4d5b39ed944ef372
bklimza <72684436+bklimza@users.noreply.github.com>
2021-01-06 13:30:22 +0100

    Update University.py

--- Lab3/University.py
+++ Lab3/University.py
@@ -1,80 +1,67 @@
-class Student:
-    def __init__(self, first_name, last_name, email, index, grades=None):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.index = index
+class Individual:
+    def __init__(self, email, name, surname):
         self.inbox = []
+        self.email = email
+        self.name = name
+        self.surname = surname
+
+    def send_message(self, text, sender):
+        self.inbox.append((text, sender))
+
+
+class Student(Individual):
+    def __init__(self, index_number, email, name, surname, grades=None):
+        self.index_number = index_number
+        Individual.__init__(self, email, name, surname)
         if grades is None:
             self.grades = dict()
         else:
             self.grades = grades
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' ' + str(self.index) + ' ' + str(self.grades)
+        return self.name + ' ' + self.surname + ' ' + self.email + ' ' + str(self.index_number) + ' ' + str(self.grades)
 
-    def add_grade(self, subject, grade: float):
+    def add_new_grade(self, subject, grade: float):
+        if (subject not in self.grades.keys()) or (grade < 2.0) or (grade > 5.0):
+            raise ValueError
         self.grades[subject] = grade
 
-    def send_email(self, text):
-        self.inbox.append(text)
+
+class UniversityEmployee(Individual):
+    def __init__(self, email, name, surname, room_number):
+        Individual.__init__(self, email, name, surname)
+        self.room_number = room_number
 
 
-class TeachingWorker:
-    def __init__(self, first_name, last_name, email, room, subjects, consultation):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.room = room
-        self.subjects = subjects
-        self.consultation = consultation
-        self.inbox = []
+class ResearchEmployee(UniversityEmployee):
+    def __init__(self, email, name, surname, room_number, publications_list):
+        UniversityEmployee.__init__(self, email, name, surname, room_number)
+        self.publications_list = publications_list
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' ' + self.email + ' ' + self.room + ' '\
-               + str(self.subjects) + ' ' + self.consultation
+        return self.name + ' ' + self.surname + ' ' + self.email + ' ' + self.room_number + ' ' \
+               + str(self.publications_list)
 
-    def send_email(self, text):
-        self.inbox.append(text)
+    def add_new_publication(self, title):
+        self.publications_list.append(title)
 
 
-class ResearchWorker:
-    def __init__(self, first_name, last_name, email, room, publications):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.room = room
-        self.publications = publications
-        self.inbox = []
+class TeachingEmployee(UniversityEmployee):
+    def __init__(self, email, name, surname, room_number, consultation_time, subjects_list):
+        UniversityEmployee.__init__(self, email, name, surname, room_number)
+        self.consultation_time = consultation_time
+        self.subjects_list = subjects_list
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' ' + self.email + ' ' + self.room + ' '\
-               + str(self.publications)
-
-    def send_email(self, text):
-        self.inbox.append(text)
-
-    def add_publications(self, publication):
-        self.publications.append(publication)
+        return self.name + ' ' + self.surname + ' ' + self.email + ' ' + self.room_number + ' ' \
+            + self.consultation_time + ' ' + str(self.subjects_list)
 
 
-class TeachingAndResearchWorker:
-    def __init__(self, first_name, last_name, email, room, publications, subjects, consultation):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.room = room
-        self.publications = publications
-        self.subjects = subjects
-        self.consultation = consultation
-        self.inbox = []
-
-    def send_email(self, text):
-        self.inbox.append(text)
-
-    def add_publications(self, publication):
-        self.publications.append(publication)
+class TeachingAndResearchEmployee(TeachingEmployee, ResearchEmployee):
+    def __init__(self, email, name, surname, room_number, consultation_time, publications_list, subjects_list):
+        TeachingEmployee.__init__(self, email, name, surname, room_number, subjects_list, consultation_time)
+        ResearchEmployee.__init__(self, email, name, surname, room_number, publications_list)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' ' + self.email + ' ' + self.room + ' '\
-               + str(self.subjects) + ' ' + self.consultation + ' ' + str(self.publications)
+        return self.name + ' ' + self.surname + ' ' + self.email + ' ' + self.room_number + ' ' \
+            + self.consultation_time + ' ' + str(self.subjects_list) + ' ' + str(self.publications_list)
