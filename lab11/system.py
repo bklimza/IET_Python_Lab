e804007f64766b5b70f8839eb79e8ab918c2c13a
bklimza <72684436+bklimza@users.noreply.github.com>
2021-01-10 22:36:18 +0100

    Update system.py

--- lab11/system.py
+++ lab11/system.py
@@ -43,7 +43,7 @@ def show_reader_menu(current_reader):
 
 def show_worker_menu(current_worker):
     while True:
-        x = input('1. Dodaj książkę\n2. Usuń książkę\n3. Dodaj czytelnika\n4. Przyjmij zwrot\n5. Wyloguj się\n')
+        x = input('1. Dodaj książkę\n2. Usuń książkę\n3. Dodaj czytelnika\n4. Przyjmij zwrot\n5. Zakończ\n')
         if x == '1':
             new_book = input('Podaj tytuł, autora, rok wydania i identyfikator (oddzielone przecinkami): ').split()
             current_worker.add_book(new_book[0], new_book[1], int(new_book[2]), new_book[3])
