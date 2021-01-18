5a5a02b289404df35d15762c8d7c4910ea8df667
bklimza <72684436+bklimza@users.noreply.github.com>
2021-01-18 20:35:05 +0100

    Update Polynomial.py

--- Lab3/Polynomial.py
+++ Lab3/Polynomial.py
@@ -12,16 +12,19 @@ class Polynomial:
             return True
 
     def __str__(self):
-        str_result = [str(self.coefficients[index]) + '*x^' + str(index) for index in range(len(self.coefficients))
-                      if self.coefficients[index] != 0]
-        return '+'.join(str_result)
+        str_result = ''
+        for index in range(len(self.coefficients)):
+            if self.coefficients[index] != 0:
+                str_result += ' + ' + str(self.coefficients[index]) + 'x**' + str(index)
+        return str_result
 
     def __add__(self, other):
         if isinstance(other, (int, float)):
             self.coefficients[0] += other
             return Polynomial(self.coefficients)
         else:
-            added = [c2 + c2 for c1, c2 in zip_longest(self.coefficients, other.coefficients)]
+            added = [correct_value(c1) + correct_value(c2)
+                     for c1, c2 in zip_longest(self.coefficients, other.coefficients)]
             return Polynomial(added)
 
     def __sub__(self, other):
@@ -29,7 +32,8 @@ class Polynomial:
             self.coefficients[0] -= other
             return Polynomial(self.coefficients)
         else:
-            subtracted = [c1 - c2 for c1, c2 in zip_longest(self.coefficients, other.coefficients)]
+            subtracted = [correct_value(c1) - correct_value(c2)
+                          for c1, c2 in zip_longest(self.coefficients, other.coefficients)]
             return Polynomial(subtracted)
 
     def __mul__(self, other):
@@ -37,7 +41,7 @@ class Polynomial:
             multiplied = [scalar * other for scalar in self.coefficients]
             return Polynomial(multiplied)
         else:
-            multiplied = Polynomial([0])
+            multiplied = Polynomial([])
             for index in range(len(other.coefficients)):
                 multiplied += Polynomial(index * [0] + [other.coefficients[index] * coefficient
                                                         for coefficient in self.coefficients])
@@ -58,6 +62,13 @@ class Polynomial:
     def calculate_value(self, x):
         calculated = [x ** index * self.coefficients[index] for index in range(len(self.coefficients))]
         return sum(calculated)
+
+
+def correct_value(value):
+    if value is None:
+        return 0
+    else:
+        return value
 
 # defaultdict sprawdziłby się, domyślna wartość mogłaby wynosić 0.
 # Dla liczb zespolonych i macierzy również by działało, o ile macierze byłyby kwadratowe i ustalonego rozmiaru.
