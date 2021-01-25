7c8a975673b50173f13d000814fee4b9b3fa9230
bklimza <72684436+bklimza@users.noreply.github.com>
2021-01-25 17:34:27 +0100

    Add files via upload

---
+++ lab3/polynomial.py
@@ -0,0 +1,63 @@
+from itertools import *
+
+
+class Polynomial:
+    def __init__(self, coefficients):
+        self.coefficients = coefficients
+
+    def __bool__(self):
+        if not any(self.coefficients):
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        polynomial_list = [str(self.coefficients[index]) + 'x**' + str(index) for index in range(len(self.coefficients))
+                           if
+                           self.coefficients[index] != 0]
+        return ' + '.join(polynomial_list)
+
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            self.coefficients[0] += other
+            return Polynomial(self.coefficients)
+        else:
+            added = [c1 + c2 for c1, c2 in zip_longest(self.coefficients, other.coefficients, fillvalue=0)]
+            return Polynomial(added)
+
+    def __sub__(self, other):
+        if isinstance(other, (int, float)):
+            self.coefficients[0] -= other
+            return Polynomial(self.coefficients)
+        else:
+            subtracted = [c1 - c2 for c1, c2 in zip_longest(self.coefficients, other.coefficients, fillvalue=0)]
+            return Polynomial(subtracted)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            multiplied = [coeff * other for coeff in self.coefficients]
+            return Polynomial(multiplied)
+        else:
+            multiplied_len = len(other.coefficients) + len(self.coefficients) - 1
+            multiplied = [0 for _ in range(multiplied_len)]
+            for i in range(len(other.coefficients)):
+                for j in range(len(self.coefficients)):
+                    multiplied[i+j] += other.coefficients[i] * self.coefficients[j]
+            return Polynomial(multiplied)
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __isub__(self, other):
+        return self - other
+
+    def __imul__(self, other):
+        return self * other
+
+    def calculate_value(self, value):
+        calculated_list = [(value ** index) * self.coefficients[index] for index in range(len(self.coefficients))]
+        return sum(calculated_list)
+
+
+# defaultdict byłby w porządku, domyślna wartość mogłaby wynosić 0.
+# Dla liczb zespolonych i macierzy również by działało, o ile macierze byłyby kwadratowe i ustalonego rozmiaru.
