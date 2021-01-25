aefb5ed0a7b203ad4c0bdca94d3e5bfc687b92d0
bklimza <72684436+bklimza@users.noreply.github.com>
2021-01-25 15:48:20 +0100

    Update Students_and_university_employees.py

--- Lab3/Students_and_university_employees.py
+++ Lab3/Students_and_university_employees.py
@@ -17,10 +17,10 @@ class Student(Individual):
         if owned_grades is None:
             self.owned_grades = dict()
         self.id_number = id_number
-        Individual.__init__(self, email, name, surname)
+        super().__init__(email, name, surname)
 
     def __str__(self):
-        return Individual.__str__(self) + ' , ' + str(self.owned_grades) + ' , ' + str(self.id_number)
+        return super().__str__() + ' , ' + str(self.owned_grades) + ' , ' + str(self.id_number)
 
     def add_new_grade(self, subject, grade: float):
         if (subject not in self.owned_grades.keys()) or (grade < 2.0) or (grade > 5.0):
@@ -30,20 +30,20 @@ class Student(Individual):
 
 class UniversityEmployee(Individual):
     def __init__(self, email, name, surname, room_number):
-        Individual.__init__(self, email, name, surname)
+        super().__init__(email, name, surname)
         self.room_number = room_number
 
     def __str__(self):
-        return Individual.__str__(self) + ' , ' + str(self.room_number)
+        return super().__str__() + ' , ' + str(self.room_number)
 
 
 class ResearchEmployee(UniversityEmployee):
     def __init__(self, email, name, surname, room_number, publications_list):
-        UniversityEmployee.__init__(self, email, name, surname, room_number)
+        super().__init__(email, name, surname, room_number)
         self.publications_list = publications_list
 
     def __str__(self):
-        return UniversityEmployee.__str__(self) + ' , ' + str(self.publications_list)
+        return super().__str__() + ' , ' + str(self.publications_list)
 
     def add_new_publication(self, title):
         self.publications_list.append(title)
@@ -51,12 +51,12 @@ class ResearchEmployee(UniversityEmployee):
 
 class TeachingEmployee(UniversityEmployee):
     def __init__(self, email, name, surname, room_number, consultation_time, subjects_list):
-        UniversityEmployee.__init__(self, email, name, surname, room_number)
+        super().__init__(email, name, surname, room_number)
         self.consultation_time = consultation_time
         self.subjects_list = subjects_list
 
     def __str__(self):
-        return UniversityEmployee.__str__(self) + ' , ' + self.consultation_time + ' , ' + str(self.subjects_list)
+        return super().__str__() + ' , ' + self.consultation_time + ' , ' + str(self.subjects_list)
 
 
 class TeachingAndResearchEmployee(TeachingEmployee, ResearchEmployee):
